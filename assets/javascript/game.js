a0e8e8b298329ec28876e6186141e6bf96d9b1dc
Jonathan Stewart <AnorLondo@Jonathans-MacBook-Air.local>
2016-04-27 23:19:31 -0400

    test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,145 +5,158 @@ var game = {
 	spaceSelected: "",
 	// 0 for Os, 1 for Xs
 	currentPlayer: 1,
-<<<<<<< HEAD
 	gameWon: false,
-	testVariableSame: true
-=======
+	testVariableSame: true,
 	exes: function (){
 		$("#"+game.spaceSelected).html('<img id="exes" src="assets/images/tic-tac-toe-X.png">')
 	},
 	ohs: function(){
 		$("#"+game.spaceSelected).html('<img id="ohs" src="assets/images/tic-tac-toe-O.png">')
 	}
-
->>>>>>> 3624a8ffd71edcc35e8936c4d07480e51bebf12e
 };
 
 $(".square").on("click", function(){
 	if(game.gameWon == false)
 	{
-<<<<<<< HEAD
 
 		game.spaceSelected = $(this).attr("id");
 
 		switch(game.spaceSelected)
 		{
 			case "1-1":
-				if(game.currentPlayer == 0)
-				{
-					game.grid[0][0] = 0;
-					game.currentPlayer = 1;
-				}
-				else
-				{
-					game.grid[0][0] = 1;
-					game.currentPlayer = 0;
-				}
-				break;
+			if(game.currentPlayer == 0)
+			{
+				game.grid[0][0] = 0;
+				game.currentPlayer = 1;
+				game.ohs();
+			}
+			else
+			{
+				game.grid[0][0] = 1;
+				game.currentPlayer = 0;
+				game.exes();
+			}
+			break;
 
 			case "1-2":
-				if(game.currentPlayer == 0)
-				{
-					game.grid[0][1] = 0;
-					game.currentPlayer = 1;
-				}
-				else
-				{
-					game.grid[0][1] = 1;
-					game.currentPlayer = 0;
-				}
-				break;
+			if(game.currentPlayer == 0)
+			{
+				game.grid[0][1] = 0;
+				game.currentPlayer = 1;
+				game.ohs();
+			}
+			else
+			{
+				game.grid[0][1] = 1;
+				game.currentPlayer = 0;
+				game.exes();
+			}
+			break;
 
 			case "1-3":
-				if(game.currentPlayer == 0)
-				{
-					game.grid[0][2] = 0;
-					game.currentPlayer = 1;
-				}
-				else
-				{
-					game.grid[0][2] = 1;
-					game.currentPlayer = 0;
-				}
-				break;
+			if(game.currentPlayer == 0)
+			{
+				game.grid[0][2] = 0;
+				game.currentPlayer = 1;
+				game.ohs();
+			}
+			else
+			{
+				game.grid[0][2] = 1;
+				game.currentPlayer = 0;
+				game.exes();
+			}
+			break;
 
 			case "2-1":
-				if(game.currentPlayer == 0)
-				{
-					game.grid[1][0] = 0;
-					game.currentPlayer = 1;
-				}
-				else
-				{
-					game.grid[1][0] = 1;
-					game.currentPlayer = 0;
-				}
-				break;
+			if(game.currentPlayer == 0)
+			{
+				game.grid[1][0] = 0;
+				game.currentPlayer = 1;
+				game.ohs();
+			}
+			else
+			{
+				game.grid[1][0] = 1;
+				game.currentPlayer = 0;
+				game.exes();
+			}
+			break;
 
 			case "2-2":
-				if(game.currentPlayer == 0)
-				{
-					game.grid[1][1] = 0;
-					game.currentPlayer = 1;
-				}
-				else
-				{
-					game.grid[1][1] = 1;
-					game.currentPlayer = 0;
-				}
-				break;
+			if(game.currentPlayer == 0)
+			{
+				game.grid[1][1] = 0;
+				game.currentPlayer = 1;
+				game.ohs();
+			}
+			else
+			{
+				game.grid[1][1] = 1;
+				game.currentPlayer = 0;
+				game.exes();
+			}
+			break;
 
 			case "2-3":
-				if(game.currentPlayer == 0)
-				{
-					game.grid[1][2] = 0;
-					game.currentPlayer = 1;
-				}
-				else
-				{
-					game.grid[1][2] = 1;
-					game.currentPlayer = 0;
-				}
-				break;
+			if(game.currentPlayer == 0)
+			{
+				game.grid[1][2] = 0;
+				game.currentPlayer = 1;
+				game.ohs();
+			}
+			else
+			{
+				game.grid[1][2] = 1;
+				game.currentPlayer = 0;
+				game.exes();
+			}
+			break;
 
 			case "3-1":
-				if(game.currentPlayer == 0)
-				{
-					game.grid[2][0] = 0;
-					game.currentPlayer = 1;
-				}
-				else
-				{
-					game.grid[2][0] = 1;
-					game.currentPlayer = 0;
-				}
-				break;
+			if(game.currentPlayer == 0)
+			{
+				game.grid[2][0] = 0;
+				game.currentPlayer = 1;
+				game.ohs();
+			}
+			else
+			{
+				game.grid[2][0] = 1;
+				game.currentPlayer = 0;
+				game.exes();
+			}
+			break;
 
 			case "3-2":
-				if(game.currentPlayer == 0)
-				{
-					game.grid[2][1] = 0;
-					game.currentPlayer = 1;
-				}
-				else
-				{
-					game.grid[2][1] = 1;
-					game.currentPlayer = 0;
-				}
-				break;
+			if(game.currentPlayer == 0)
+			{
+				game.grid[2][1] = 0;
+				game.currentPlayer = 1;
+				game.ohs();
+			}
+			else
+			{
+				game.grid[2][1] = 1;
+				game.currentPlayer = 0;
+				game.exes();
+			}
+			break;
 
 			case "3-3":
-				if(game.currentPlayer == 0)
-				{
-					game.grid[2][2] = 0;
-					game.currentPlayer = 1;
-				}
-				else
-				{
-					game.grid[2][2] = 1;
-					game.currentPlayer = 0;
-				}
-				break;
+			if(game.currentPlayer == 0)
+			{
+				game.grid[2][2] = 0;
+				game.currentPlayer = 1;
+				game.ohs();
+			}
+			else
+			{
+				game.grid[2][2] = 1;
+				game.currentPlayer = 0;
+				game.exes();
+			}
+			break;
 
 			default:
 				alert("Something broke!");
@@ -170,144 +183,6 @@ $(".square").on("click", function(){
 				}
 			}
 		}
-=======
-		case "1-1":
-			if(game.currentPlayer == 0)
-			{
-				game.grid[0][0] = 0;
-				game.currentPlayer = 1;
-				game.ohs();
-			}
-			else
-			{
-				game.grid[0][0] = 1;
-				game.currentPlayer = 0;
-				game.exes();
-			}
-			break;
 
-		case "1-2":
-			if(game.currentPlayer == 0)
-			{
-				game.grid[0][1] = 0;
-				game.currentPlayer = 1;
-				game.ohs();
-			}
-			else
-			{
-				game.grid[0][1] = 1;
-				game.currentPlayer = 0;
-				game.exes();
-			}
-			break;
-
-		case "1-3":
-			if(game.currentPlayer == 0)
-			{
-				game.grid[0][2] = 0;
-				game.currentPlayer = 1;
-				game.ohs();
-			}
-			else
-			{
-				game.grid[0][2] = 1;
-				game.currentPlayer = 0;
-				game.exes();
-			}
-			break;
-
-		case "2-1":
-			if(game.currentPlayer == 0)
-			{
-				game.grid[1][0] = 0;
-				game.currentPlayer = 1;
-				game.ohs();
-			}
-			else
-			{
-				game.grid[1][0] = 1;
-				game.currentPlayer = 0;
-				game.exes();
-			}
-			break;
-
-		case "2-2":
-			if(game.currentPlayer == 0)
-			{
-				game.grid[1][1] = 0;
-				game.currentPlayer = 1;
-				game.ohs();
-			}
-			else
-			{
-				game.grid[1][1] = 1;
-				game.currentPlayer = 0;
-				game.exes();
-			}
-			break;
-
-		case "2-3":
-			if(game.currentPlayer == 0)
-			{
-				game.grid[1][2] = 0;
-				game.currentPlayer = 1;
-				game.ohs();
-			}
-			else
-			{
-				game.grid[1][2] = 1;
-				game.currentPlayer = 0;
-				game.exes();
-			}
-			break;
-
-		case "3-1":
-			if(game.currentPlayer == 0)
-			{
-				game.grid[2][0] = 0;
-				game.currentPlayer = 1;
-				game.ohs();
-			}
-			else
-			{
-				game.grid[2][0] = 1;
-				game.currentPlayer = 0;
-				game.exes();
-			}
-			break;
-
-		case "3-2":
-			if(game.currentPlayer == 0)
-			{
-				game.grid[2][1] = 0;
-				game.currentPlayer = 1;
-				game.ohs();
-			}
-			else
-			{
-				game.grid[2][1] = 1;
-				game.currentPlayer = 0;
-				game.exes();
-			}
-			break;
-
-		case "3-3":
-			if(game.currentPlayer == 0)
-			{
-				game.grid[2][2] = 0;
-				game.currentPlayer = 1;
-				game.ohs();
-			}
-			else
-			{
-				game.grid[2][2] = 1;
-				game.currentPlayer = 0;
-				game.exes();
-			}
-			break;
-
-		default:
-			alert("Something broke!");
->>>>>>> 3624a8ffd71edcc35e8936c4d07480e51bebf12e
 	}
 });
