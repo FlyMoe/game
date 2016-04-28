60aefe5703fe315dfa01a27aff78d63e67b14361
Kyle Rinehart <Kyle Rinehart>
2016-04-27 21:00:28 -0400

    updated js.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var game = {
 //  0 for play, 1 for game over	
-	states: 0
+	states: 0,
 	grid : [ [" ", " ", " "], [" ", " ", " "], [" ", " ", " "] ],
 	spaceSelected: "",
 	// 0 for Os, 1 for Xs
@@ -18,117 +18,117 @@ $(".square").on("click", function(){
 		case "1-1":
 			if(game.currentPlayer == 0)
 			{
-				game.grid[1][1] = 0;
-				currentPlayer = 1;
+				game.grid[0][0] = 0;
+				game.currentPlayer = 1;
 			}
 			else
 			{
-				game.grid[1][1] = 1;
-				currentPlayer = 0;
+				game.grid[0][0] = 1;
+				game.currentPlayer = 0;
 			}
 			break;
 
 		case "1-2":
 			if(game.currentPlayer == 0)
 			{
-				game.grid[1][2] = 0;
-				currentPlayer = 1;
+				game.grid[0][1] = 0;
+				game.currentPlayer = 1;
 			}
 			else
 			{
-				game.grid[1][2] = 1;
-				currentPlayer = 0;
+				game.grid[0][1] = 1;
+				game.currentPlayer = 0;
 			}
 			break;
 
 		case "1-3":
 			if(game.currentPlayer == 0)
 			{
-				game.grid[1][3] = 0;
-				currentPlayer = 1;
+				game.grid[0][2] = 0;
+				game.currentPlayer = 1;
 			}
 			else
 			{
-				game.grid[1][3] = 1;
-				currentPlayer = 0;
+				game.grid[0][2] = 1;
+				game.currentPlayer = 0;
 			}
 			break;
 
 		case "2-1":
 			if(game.currentPlayer == 0)
 			{
-				game.grid[2][1] = 0;
-				currentPlayer = 1;
+				game.grid[1][0] = 0;
+				game.currentPlayer = 1;
 			}
 			else
 			{
-				game.grid[2][1] = 1;
-				currentPlayer = 0;
+				game.grid[1][0] = 1;
+				game.currentPlayer = 0;
 			}
 			break;
 
 		case "2-2":
 			if(game.currentPlayer == 0)
 			{
-				game.grid[2][2] = 0;
-				currentPlayer = 1;
+				game.grid[1][1] = 0;
+				game.currentPlayer = 1;
 			}
 			else
 			{
-				game.grid[2][2] = 1;
-				currentPlayer = 0;
+				game.grid[1][1] = 1;
+				game.currentPlayer = 0;
 			}
 			break;
 
 		case "2-3":
 			if(game.currentPlayer == 0)
 			{
-				game.grid[2][3] = 0;
-				currentPlayer = 1;
+				game.grid[1][2] = 0;
+				game.currentPlayer = 1;
 			}
 			else
 			{
-				game.grid[2][3] = 1;
-				currentPlayer = 0;
+				game.grid[1][2] = 1;
+				game.currentPlayer = 0;
 			}
 			break;
 
 		case "3-1":
 			if(game.currentPlayer == 0)
 			{
-				game.grid[3][1] = 0;
-				currentPlayer = 1;
+				game.grid[2][0] = 0;
+				game.currentPlayer = 1;
 			}
 			else
 			{
-				game.grid[3][1] = 1;
-				currentPlayer = 0;
+				game.grid[2][0] = 1;
+				game.currentPlayer = 0;
 			}
 			break;
 
 		case "3-2":
 			if(game.currentPlayer == 0)
 			{
-				game.grid[3][2] = 0;
-				currentPlayer = 1;
+				game.grid[2][1] = 0;
+				game.currentPlayer = 1;
 			}
 			else
 			{
-				game.grid[3][2] = 1;
-				currentPlayer = 0;
+				game.grid[2][1] = 1;
+				game.currentPlayer = 0;
 			}
 			break;
 
 		case "3-3":
 			if(game.currentPlayer == 0)
 			{
-				game.grid[3][3] = 0;
-				currentPlayer = 1;
+				game.grid[2][2] = 0;
+				game.currentPlayer = 1;
 			}
 			else
 			{
-				game.grid[3][3] = 1;
-				currentPlayer = 0;
+				game.grid[2][2] = 1;
+				game.currentPlayer = 0;
 			}
 			break;
 
